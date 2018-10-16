73b851c51b4061db2951288ca7870a1e617dd61d
Concross <cacrossley22@gmail.com>
2018-10-15 18:33:57 -0700

    add cowsay to app component

--- src/components/app.js
+++ src/components/app.js
@@ -4,17 +4,36 @@ import React from 'react';
 
 import Header from './header/header';
 import Footer from './footer/footer';
+import { say } from 'cowsay';
 
 import '../style/app.scss';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: `moo`,
+    };
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    let message = say({
+      text: `I'm a cow, MOOO!`,
+    });
+
+    this.setState({ message });
+  }
 
   render() {
     return (
       <React.Fragment>
         <Header title="Generate Cowsay Lorem" />
         <main>
-          <p>I am a cow</p>
+          <button onClick={this.updateState}>Moo</button>
+          <pre>{this.state.message}</pre>
         </main>
         <Footer />
       </React.Fragment>
